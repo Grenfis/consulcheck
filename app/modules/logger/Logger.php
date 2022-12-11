949f2fbf3f7881a3fd591c4b84de4d0810d1b034
Viktor Kubli <vkubli@stat4market.com>
2022-12-11 13:24:43 +0400

    [feat] added logger

---
+++ app/modules/logger/Logger.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\modules\logger;
+
+use app\system\Common;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger as MonologLogger;
+
+class Logger
+{
+    private static ?self $instance;
+
+    private MonologLogger $logger;
+
+    public function __construct()
+    {
+        $this->logger = new MonologLogger('main');
+
+        $pathParts = [
+            Common::getRoot(),
+            '..',
+            'logs',
+            $this->logger->getName() . '.log'
+        ];
+
+        $this->logger->pushHandler(
+            new StreamHandler(implode(DIRECTORY_SEPARATOR, $pathParts), MonologLogger::INFO)
+        );
+    }
+
+    public static function instance(): self
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function warning(string $msg)
+    {
+        $this->logger->warning($msg);
+    }
+}
